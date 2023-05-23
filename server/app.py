444d5a029e824bea14a16111b180edfc01aec49b
Angela Palaszewski <delarosa.ang@gmail.com>
2023-05-22 23:06:46 -0600

    complete lab

--- server/app.py
+++ server/app.py
@@ -19,15 +19,34 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.filter(Animal.id == id).first()
+    return f'<ul>Name: {animal.name}</ul>' \
+    + f'<ul>Species: {animal.species}</ul>' \
+    + f'<ul>Zookeeper: {animal.zookeeper.name}</ul>' \
+    + f'<ul>Enclosure: {animal.enclosure.environment}</ul>'
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
+    response_body = f'<ul>Name: {zookeeper.name}</ul>' \
+    + f'<ul>Birthday: {zookeeper.birthday}</ul>' \
+    
+    for animal in zookeeper.animals:
+        response_body += f'<ul>Animal: {animal.name}</ul>'
+
+    return make_response(response_body)
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.filter(Enclosure.id == id).first()
+    response_body =  f'<ul>Environment: {enclosure.environment}</ul>' \
+    + f'<ul>Open to Visitors: {enclosure.open_to_visitors}</ul>' \
+    
+    for animal in enclosure.animals:
+        response_body += f'<ul>Animal: {animal.name}</ul>'
+
+    return make_response(response_body)
+    
 
 
 if __name__ == '__main__':
